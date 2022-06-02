c89c05ad9070610b11680696dc53f5bc9cb52225
Billy <bbray@cloudentity.com>
2022-06-02 09:07:44 -0400

    added readme

--- SimplePkce/app/src/main/java/com/example/simplepkce/util/Config.kt
+++ SimplePkce/app/src/main/java/com/example/simplepkce/util/Config.kt
@@ -1,9 +1,10 @@
 package com.example.simplepkce.util
 
 object Config {
-    const val BASE_URL = "https://cloudentity-bbray2.us.authz.cloudentity.io"
-    const val AUTHORIZE_ENDPOINT = "https://cloudentity-bbray2.us.authz.cloudentity.io/cloudentity-bbray2/mobile/oauth2/authorize"
-    const val CLIENT_ID = "ca2i0ah972lv90m7jdi0"
-    const val REDIRECT_URI = "oauth://simple-pkce.example.com"
+    const val BASE_URL = "<base URL for your tenant>"
+    const val AUTHORIZE_ENDPOINT = "<authorize endpoint for your Cloudentity client application>"
+    const val TOKEN_ENDPOINT = "<token endpoint for your Cloudentity client application>"
+    const val CLIENT_ID = "<client ID for your Cloudentity client application>"
+    const val REDIRECT_URI = "oauth://simple-pkce.example.com" // if edited, update your Cloudentity client application redirect_uri
     const val GRANT_TYPE = "authorization_code"
 }
